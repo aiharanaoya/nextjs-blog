ce99ae1d299292905e29f5cedff5b7fa0f2533c5
aiharanaoya <nao11aihara@gmail.com>
2022-08-02 01:05:38 +0900

    feat: 動的ルート

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import Layout from "../components/layout";
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -14,19 +17,25 @@ export async function getStaticProps() {
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      {/* Keep the existing code here */}
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
 
-      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={utilStyles.headingMd}>
+        <p>Your Self Introduction</p>
+      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
